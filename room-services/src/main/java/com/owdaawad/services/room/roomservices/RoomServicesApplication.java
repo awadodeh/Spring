7d831fa1a2b577c517992825cc3bd702c5c658e1
owdaa <awadodeh@gmail.com>
2017-10-12 14:42:30 -0500

    adding room-service

---
+++ room-services/src/main/java/com/owdaawad/services/room/roomservices/RoomServicesApplication.java
@@ -0,0 +1,37 @@
+package com.owdaawad.services.room.roomservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.any;
+
+@SpringBootApplication
+@EnableSwagger2
+public class RoomServicesApplication {
+
+	@Bean
+	public Docket api(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("Room")
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.owdaawad.services.room"))
+				.paths(any())
+				.build()
+				.apiInfo(new ApiInfo("Room Services",
+						"A set of services to provide data access to rooms", "1.0.0", null,
+//						new Contact("Awad Owda", "https://www.facebook.com/awad.odeh2", "awadodeh@gmail.com").toString()
+						"Awad Owda"
+						,null, null));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomServicesApplication.class, args);
+	}
+}
